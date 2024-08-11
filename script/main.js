751cab85d2e72959b1420fca0ddee387a10127f4
Satraniel <157123095+Satraniel@users.noreply.github.com>
2024-08-12 00:21:09 +0700

    main.js

--- script/main.js
+++ script/main.js
@@ -116,8 +116,8 @@ const animationTimeline = () => {
     "+=1")
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
+    .from(".idea-2", 1.7, ideaTextTrans)
+    .to(".idea-2", 1.7, ideaTextTransLeave, "+=4.5")
     .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
         scale: 1.2,
